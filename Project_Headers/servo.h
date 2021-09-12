8e69f58eefd7103e68db344a2adc0653800c4f63
eladsofer879 <36042562+eladsofer879@users.noreply.github.com>
2021-09-12 16:18:35 +0300

    change servo degree scale

--- Project_Headers/servo.h
+++ Project_Headers/servo.h
@@ -15,9 +15,10 @@ void InitServo();
 void InitSensors();
 void WriteServo(int deg);
 void SweepServo();
-#define MOTOR_MUDULO_REGISTER  9375
-#define MIN_T_ON 0.6
-#define MAX_T_ON 2.5
+
+#define MOTOR_MUDULO_REGISTER  9373 //   40Hz = 24Mhz/(64x9375) 64 is the prescaler. 
+#define MIN_T_ON 0.6 // minimum Minimum Duty cycle in MS
+#define MAX_T_ON 2.3 // suppose to be 2.5, although 0.2 is an offset for maximum accuracy 
 #define WAVE_PERIOD 25.0
 #define MAX_SERVO_DEGREE 180.0
 #define MIN_SERVO_DEGREE 0
